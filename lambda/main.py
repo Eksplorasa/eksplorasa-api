45eb6d608f7c7495a1a5207e63e7bfa5d64c080f
BenedictPresley <19623252@std.stei.itb.ac.id>
2025-08-06 00:53:17 +0700

    init

---
+++ lambda/main.py
@@ -0,0 +1,10 @@
+import os
+
+def handler(event, context):
+	version = os.environ.get("VERSION", "0.0")
+	response_body = {
+		"message": "Hello from Lambda!",
+		"version": version,
+	}
+	
+	return {"statusCode": 200, "body": response_body}
